f2c539418b1125c2c7bde16ae4a3853ab327b1d6
YUDAMODS <119145280+YUDAMODS@users.noreply.github.com>
2024-05-14 09:22:36 +0700

    Add config.js

---
+++ config.js
@@ -0,0 +1,63 @@
+/*
+  ( Base Ori WannOFFC°GhostMods°GlobalElits )
+
+Script Ini Murni Bikinan Sendiri, Saya Hanya Sekedar Kroco Penghuni Inti Bumi.
+
+Note : 
+• Jangan Membagikan Script Ini Secara Free !!
+• Jangan Mengganti Name Developer Script !!
+• Jangan Menjual Script Ini Di Atas Harga Yang Saya Jual !!
+
+Thanks To :                                
+- Allah Swt 
+- Nabi Muhammad Saw         
+- My Team
+- WannOFFC   :   [ Support ] 
+- GlobalElits       [ Owner ]
+- Ghost       :    [ Support ] 
+- Pengguna Bot Yang Selalu Support
+
+* Nama Pengembang: MR.Felix 
+* Kontak Whatsapp: wa.me/62856932854302
+* Kontak Telegram: t.me/Globalelits
+* Akun Github: github.com/Felixhasgawa
+* Catatan: Skrip ini tidak Geratis.
+
+• Recode By ( Nama Mu )
+*/
+module.exports = {
+  
+//___________ ┏  Penting Settings  ┓ ___________\\
+  token: '7159926680:AAED2P8bQIVVVUYCJHIstposxO_WX6yTV2g', 
+  adminId: '6991457866',
+  adminUrl: 'https://t.me/YUDAMODS',
+  testi: 'https://t.me/twstimoni',
+  mur: 'https://t.me/muridbanunban',
+  pro: 'https://t.me/produk-bokep', 
+    
+//___________ ┏  API Settings  ┓ ___________\\
+  apilol: '76fffde439b25dbb5cf17eef', // api.lolhuman.xyz
+  apibot: 'beta-feelixsuden', // api.betabotz.eu.org
+  apibeta: 'felixganz', // api.botcahx.eu.org
+
+//___________ ┏  Cpanel Settings1  ┓ ___________\\
+  domain: 'https://panel.roomdigital.site', // isi pake link bokep juga bagus :V
+  plta: 'ptla_XoWR5PQFCVYZdrxawm3CY4PPA0dkMvMwustdTe2XCeS',
+  pltc: 'ptlc_2PQmoIsyeqtyheKOO0HYnwdvC3ym6y1khz6OQMyH5Hp',
+
+//___________ ┏  Cpanel Settings2  ┓ ___________\\
+  domain2: 'https://panel.roomdigital.site', // isi pake link bokep juga bagus :V
+  plta2: 'ptla_XoWR5PQFCVYZdrxawm3CY4PPA0dkMvMwustdTe2XCeS',
+  pltc2: 'ptlc_2PQmoIsyeqtyheKOO0HYnwdvC3ym6y1khz6OQMyH5Hp',
+
+//___________ ┏  Eggs Settings All  ┓ ___________\\
+  loc: '1', // samain ama domian yang ada di domain
+  eggs: '15', // samain ama domian yang ada di domain
+  
+//___________ ┏  FOTO Settings  ┓ ___________\\
+  menu: 'https://telegra.ph/file/9d960a75c197e23b725b8.jpg',
+  menu2: 'https://telegra.ph/file/9d960a75c197e23b725b8.jpg',
+  qris: 'https://telegra.ph/file/91004b75ad932d0455c6a.jpg',
+  dana: 'https://telegra.ph/file/91004b75ad932d0455c6a.jpg',
+  error: 'https://www.google.com/imgres?q=error%20404&imgurl=https%3A%2F%2Fimg.freepik.com%2Ffree-vector%2Fglitch-error-404-page_23-2148105404.jpg&imgrefurl=https%3A%2F%2Fwww.freepik.com%2Ffree-vector%2Fglitch-error-404-page_4121421.htm&docid=qppMtPufbfNxQM&tbnid=4iNXAkUlSgG7BM&vet=12ahUKEwiVn5XNxseFAxXw4DgGHYrZD6QQM3oECEwQAA..i&w=626&h=626&hcb=2&ved=2ahUKEwiVn5XNxseFAxXw4DgGHYrZD6QQM3oECEwQAA',
+};
