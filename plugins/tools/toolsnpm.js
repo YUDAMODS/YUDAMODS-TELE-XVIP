beec371a3e60fc15f050eca67e02a18a7869758b
YUDAMODS <119145280+YUDAMODS@users.noreply.github.com>
2024-05-14 09:24:36 +0700

    Add toolsnpm.js

---
+++ plugins/tools/toolsnpm.js
@@ -0,0 +1,38 @@
+/*
+  ( Base Ori WannOFFC°GhostMods°GlobalElits )
+
+Script Ini Murni Bikinan Sendiri, Saya Hanya Sekedar Kroco Penghuni Inti Bumi.
+
+Note : 
+• Jangan Membagikan Script Ini Secara Free !!
+• Jangan Mengganti Name Developer Script !!
+• Jangan Menjual Script Ini Di Atas Harga Yang Saya Jual !!
+
+Thanks To :                                
+- Allah Swt 
+- Nabi Muhammad Saw         
+- My Team
+- WannOFFC   :   [ My Self ] 
+- GlobalElits       [ Support ]
+- Ghost       :    [ Support ] 
+- Pengguna Bot Yang Selalu Support
+
+• Recode By ( Nama Mu )
+*/
+const fetch = require('node-fetch') 
+async function handlenpm(bot) {
+	
+bot.onText(/\.npm(.+)/, async (msg, match) => {
+    const chatId = msg.chat.id;
+    const nama = match[1].trim();
+	if (!nama) throw 'Input Query'
+	let res = await fetch(`http://registry.npmjs.com/-/v1/search?text=${nama}`)
+	let { objects } = await res.json()
+	if (!objects.length) throw `Query "${text}" not found :/`
+	let txt = objects.map(({ package: pkg }) => {
+		return `*${pkg.name}* (v${pkg.version})\n_${pkg.links.npm}_\n_${pkg.description}_`
+	}).join`\n\n`
+	bot.sendMessage(chatId, txt)
+});
+}
+module.exports = handlenpm;
