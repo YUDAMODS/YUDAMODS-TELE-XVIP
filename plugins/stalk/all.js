79620653f5d04ef47e7de370a130d48cce3c911e
YUDAMODS <119145280+YUDAMODS@users.noreply.github.com>
2024-05-14 09:24:53 +0700

    Add all.js

---
+++ plugins/stalk/all.js
@@ -0,0 +1,21 @@
+/*
+  ( Base Ori WannOFFC°GhostMods°GlobalElits )
+
+Script Ini Murni Bikinan Sendiri, Saya Hanya Sekedar Kroco Penghuni Inti Bumi.
+
+Note : 
+• Jangan Membagikan Script Ini Secara Free !!
+• Jangan Mengganti Name Developer Script !!
+• Jangan Menjual Script Ini Di Atas Harga Yang Saya Jual !!
+
+Thanks To :                                
+- Allah Swt 
+- Nabi Muhammad Saw         
+- My Team
+- WannOFFC   :   [ My Self ] 
+- GlobalElits       [ Support ]
+- Ghost       :    [ Support ] 
+- Pengguna Bot Yang Selalu Support
+
+• Recode By ( Nama Mu )
+*/
