153813c84632079884e62b009ad95f09449ffeba
YUDAMODS <119145280+YUDAMODS@users.noreply.github.com>
2024-05-14 09:24:53 +0700

    Add randomblackpink.js

---
+++ plugins/stalk/randomblackpink.js
@@ -0,0 +1,43 @@
+/*
+  ( Base Ori WannOFFC°GhostMods°GlobalElits )
+
+Script Ini Murni Bikinan Sendiri, Saya Hanya Sekedar Kroco Penghuni Inti Bumi.
+
+Note : 
+• Jangan Membagikan Script Ini Secara Free !!
+• Jangan Mengganti Name Developer Script !!
+• Jangan Menjual Script Ini Di Atas Harga Yang Saya Jual !!
+
+Thanks To :                                
+- Allah Swt 
+- Nabi Muhammad Saw         
+- My Team
+- WannOFFC   :   [ My Self ] 
+- GlobalElits       [ Support ]
+- Ghost       :    [ Support ] 
+- Pengguna Bot Yang Selalu Support
+
+• Recode By ( Nama Mu )
+*/
+const fetch = require('node-fetch');
+const config = require('../../config');
+// Memisahkan URL dan API key ke dalam variabel konfigurasi
+const API_KEY = config.apilol;
+
+module.exports = (bot) => {
+  bot.onText(/^\/blackpink$/, async (msg) => {
+    const chatId = msg.chat.id;
+    const apiUrl = `https://api.lolhuman.xyz/api/random/blackpink?apikey=${API_KEY}&${Math.random()}`;
+
+    try {
+      const response = await fetch(apiUrl);
+      const { url } = await response.json();
+      bot.sendPhoto(chatId, url);
+    } catch (error) {
+      console.error(error);
+      text = 'Maaf, terjadi kesalahan saat mengambil data.';
+    
+              bot.sendPhoto(msg.chat.id, config.error, { caption: text, });
+    }
+  });
+};
