38fbe25c2b2d9460e212833efb88422d69e8481b
YUDAMODS <119145280+YUDAMODS@users.noreply.github.com>
2024-05-14 09:22:51 +0700

    Add feets.js

---
+++ plugins/download/feets.js
@@ -0,0 +1,39 @@
+/*
+  ( Base Ori WannOFFC°GhostMods°GlobalElits )
+
+Script Ini Murni Bikinan Sendiri, Saya Hanya Sekedar Kroco Penghuni Inti Bumi.
+
+Note : 
+• Jangan Membagikan Script Ini Secara Free !!
+• Jangan Mengganti Name Developer Script !!
+• Jangan Menjual Script Ini Di Atas Harga Yang Saya Jual !!
+
+Thanks To :                                
+- Allah Swt 
+- Nabi Muhammad Saw         
+- My Team
+- WannOFFC   :   [ Support ] 
+- GlobalElits       [ Owner Script ]
+- Ghost       :    [ Support ] 
+- Pengguna Bot Yang Selalu Support
+
+• Recode By ( Nama Mu )
+*/
+const axios = require('axios')
+config = require('../../config');
+const asupan = config.apilol;
+async function handlefeets(bot) {
+  bot.onText(/^\.feets$/, async (msg) => {
+    let res = await axios.get(`https://api.lolhuman.xyz/api/random/nsfw/feets?apikey=${asupan}`, { responseType: 'arraybuffer' })
+    try{
+      let image = Buffer.from(res.data, 'binary')
+      bot.sendPhoto(msg.chat.id, image, {
+        caption: `ngebokep terus lu su tobat gblk`
+      });
+    } catch (err) {
+      console.log(err)
+      bot.sendMesaage(msg.chat.id, 'An error occurred while processing your request.')
+    }
+  });
+  }
+  module.exports = handlefeets;
