0b677ecb73c3a1abd06a49c19f5f0039c35afd6f
YUDAMODS <119145280+YUDAMODS@users.noreply.github.com>
2024-05-14 09:22:33 +0700

    Add index2.js

---
+++ index2.js
@@ -0,0 +1,29 @@
+/*
+  ( Base Ori WannOFFC°GhostMods°GlobalElits )
+
+Script Ini Murni Bikinan Sendiri, Saya Hanya Sekedar Kroco Penghuni Inti Bumi.
+
+Note : 
+• Jangan Membagikan Script Ini Secara Free !!
+• Jangan Mengganti Name Developer Script !!
+• Jangan Menjual Script Ini Di Atas Harga Yang Saya Jual !!
+
+Thanks To :                                
+- Allah Swt 
+- Nabi Muhammad Saw         
+- My Team
+- WannOFFC   :   [ Support ] 
+- GlobalElits       [ Owner ]
+- Ghost       :    [ Support ] 
+- Pengguna Bot Yang Selalu Support
+
+* Nama Pengembang: MR.Felix 
+* Kontak Whatsapp: wa.me/62856932854302
+* Kontak Telegram: t.me/Globalelits
+* Akun Github: github.com/Felixhasgawa
+* Catatan: Skrip ini tidak Geratis.
+
+• Recode By ( Nama Mu )
+*/
+// jangan di buka encnya yaa
+const _0x742a4a=_0x2557;function _0x2f95(){const _0xe91235=['slice','Error\x20writing\x20log\x20file:','560WYwCGp','91917mKSWWy','YYYY-MM-DD_HH-mm-ss','Exited\x20with\x20code:\x20','YudaMods\x20Bot\x20Is\x20Runing','blue','11580KIhqwL','child_process','RPG\x20BOT\x20TELEGRAM','Error\x20starting\x20bot:','Restarting...','cfonts','5012132CSHPIC','isMaster','error','Error\x20log\x20saved\x20to:\x20','say','center','format','stderr','./temp/','join','Cluster\x20Master\x20(Id\x20',')\x20is\x20running','ipc','1469wqwCCS','\x0aError\x20Stack:\x20','toString','361128xsTsJk','F\x20E\x20L\x20I\x20X','5FWvBAg','6286QcDDZs','error_log_','unwatchFile','3187416UpwmJq','path','2RZimKk','.txt','Worker\x20(Id\x20','15563660oFMBxM','green','exit','./plugins/tools/global','No\x20error\x20stack\x20available','inherit','process','log','pipe','pid','PORT','1354894kdAkef','console','11UDDRqZ','argv','fork'];_0x2f95=function(){return _0xe91235;};return _0x2f95();}(function(_0x46e7ed,_0x57c5fd){const _0x508437=_0x2557,_0x5a2235=_0x46e7ed();while(!![]){try{const _0x22b78d=-parseInt(_0x508437(0x6d))/0x1*(parseInt(_0x508437(0x98))/0x2)+parseInt(_0x508437(0x96))/0x3+-parseInt(_0x508437(0x80))/0x4*(parseInt(_0x508437(0x92))/0x5)+-parseInt(_0x508437(0x7a))/0x6*(parseInt(_0x508437(0x93))/0x7)+parseInt(_0x508437(0x74))/0x8*(-parseInt(_0x508437(0x75))/0x9)+-parseInt(_0x508437(0x9b))/0xa*(-parseInt(_0x508437(0x6f))/0xb)+parseInt(_0x508437(0x90))/0xc*(parseInt(_0x508437(0x8d))/0xd);if(_0x22b78d===_0x57c5fd)break;else _0x5a2235['push'](_0x5a2235['shift']());}catch(_0x5b30c3){_0x5a2235['push'](_0x5a2235['shift']());}}}(_0x2f95,0xeb39b));const moment=require('moment'),cluster=require('cluster'),{spawn}=require(_0x742a4a(0x7b)),path=require(_0x742a4a(0x97)),fs=require('fs'),functions=require(_0x742a4a(0x9e)),CFonts=require(_0x742a4a(0x7f)),PORT=process['env'][_0x742a4a(0x6c)]||0x1f90;let isRunning=![];if(cluster[_0x742a4a(0x81)]){console['log'](_0x742a4a(0x8a)+process[_0x742a4a(0x6b)]+_0x742a4a(0x8b)),CFonts[_0x742a4a(0x84)](_0x742a4a(0x91),{'font':'chrome','align':'center','gradient':['blue',_0x742a4a(0x9c)]}),CFonts['say'](_0x742a4a(0x7c),{'font':_0x742a4a(0x6e),'align':_0x742a4a(0x85),'gradient':[_0x742a4a(0x79),'green']});const numWorkers=process[_0x742a4a(0x70)][0x2]||0x1;for(let i=0x0;i<numWorkers;i++){cluster[_0x742a4a(0x71)]();}cluster['on'](_0x742a4a(0x9d),(_0x173658,_0x246b03,_0x3a634b)=>{const _0x201858=_0x742a4a;console[_0x201858(0x69)](_0x201858(0x9a)+_0x173658[_0x201858(0x68)][_0x201858(0x6b)]+')\x20died'),cluster['fork']();});}else start('./main.js');function _0x2557(_0x256ae1,_0x38e97c){const _0x2f95d4=_0x2f95();return _0x2557=function(_0x2557ee,_0x1ac2f6){_0x2557ee=_0x2557ee-0x68;let _0x49bd1e=_0x2f95d4[_0x2557ee];return _0x49bd1e;},_0x2557(_0x256ae1,_0x38e97c);}async function start(_0x282809){const _0x39e575=_0x742a4a;if(isRunning)return;if(!await functions())return;isRunning=!![];const _0x6806a7=[path[_0x39e575(0x89)](__dirname,_0x282809),...process[_0x39e575(0x70)][_0x39e575(0x72)](0x2)],_0x4713f4=spawn(process['argv'][0x0],_0x6806a7,{'stdio':[_0x39e575(0x6a),_0x39e575(0xa0),_0x39e575(0x6a),_0x39e575(0x8c)]});let _0x5ba320='';_0x4713f4[_0x39e575(0x87)]['on']('data',_0xbeda9d=>{const _0x4a08be=_0x39e575;_0x5ba320+=_0xbeda9d[_0x4a08be(0x8f)]();}),_0x4713f4['on'](_0x39e575(0x9d),_0x2e844c=>{const _0x13908b=_0x39e575;isRunning=![],console[_0x13908b(0x69)](_0x13908b(0x77)+_0x2e844c);if(_0x2e844c!==null){const _0x269cbc=moment()[_0x13908b(0x86)](_0x13908b(0x76)),_0x1b4c7c=_0x13908b(0x94)+_0x269cbc+_0x13908b(0x99),_0x19e052=path['join'](__dirname,_0x13908b(0x88)+_0x1b4c7c),_0xfb529a='Exit\x20Code:\x20'+_0x2e844c+_0x13908b(0x8e)+(_0x5ba320||_0x13908b(0x9f));fs[_0x13908b(0x95)](_0x6806a7[0x0]),fs['writeFile'](_0x19e052,_0xfb529a,_0x51187a=>{const _0x41893b=_0x13908b;if(_0x51187a)console[_0x41893b(0x82)](_0x41893b(0x73),_0x51187a);console[_0x41893b(0x69)](_0x41893b(0x83)+_0x19e052);});}else console[_0x13908b(0x69)](_0x13908b(0x7e)),start(_0x282809);}),_0x4713f4['on'](_0x39e575(0x82),_0x551c4c=>{const _0x2694d1=_0x39e575;isRunning=![],console[_0x2694d1(0x82)](_0x2694d1(0x7d),_0x551c4c);}),console[_0x39e575(0x69)](_0x39e575(0x78));}
